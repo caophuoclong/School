3c3dea24f39ff0a6e78832046c990902b0c94e7c
PhuocLong <phuoclong@PhuocLongs-MacBook-Pro.local>
2020-11-23 12:23:59 +0700

    them chú thích hàm

--- CT177/linked_list.c
+++ CT177/linked_list.c
@@ -7,11 +7,11 @@ struct Node {
 };
 typedef struct Node *Position;
 typedef Position List; 
-void makenullList(List *L){
+void makenullList(List *L){ // Khởi tạo danh sách rỗng
   (*L) = (struct Node*)malloc(sizeof(struct Node));
   (*L)->Next  = NULL;
 };
-int member(ElementType x, List L){
+int member(ElementType x, List L){ // Kiểm tra tồn tại của phần tử trong danh sách
   while(L->Next != NULL){
     if (x == L->Next->Element)
       return 1;
@@ -19,7 +19,7 @@ int member(ElementType x, List L){
   }
   return 0;
 }
-void append(ElementType x, List *L){
+void append(ElementType x, List *L){ // Thêm phần tử vào cuối
   Position p;
   List P;
   makenullList(&P);
@@ -31,7 +31,7 @@ void append(ElementType x, List *L){
   }
   p->Next = P;
 }
-Position locate(ElementType x, List L){
+Position locate(ElementType x, List L){ // Tìm vị trí đầu tiên của danh sách
   Position p = L;
   while(p->Next != NULL){
     if (p->Next->Element == x)
@@ -41,14 +41,15 @@ Position locate(ElementType x, List L){
   }
   return p;
 }
-void addFirst(ElementType x, List *L){
+void addFirst(ElementType x, List *L){ // Thêm phần tử vào đầu danh sách
   Position p;
+  makenullList(&p);
   p->Element = x;
   p->Next = NULL;
   p->Next = (*L) ->Next;
   (*L) ->Next = p;
 }
-List intersectionSet(List L1, List L2){
+List intersectionSet(List L1, List L2){ // Giao 2 tập hợp
   List L3;
   makenullList(&L3);
   while(L1->Next  != NULL){
@@ -59,7 +60,7 @@ List intersectionSet(List L1, List L2){
   }
   return L3;
 }
-List unionSet(List L1, List L2){
+List unionSet(List L1, List L2){ // Hợp 2 tập hợp
   while(L2->Next != NULL){
     if(!member(L2->Next->Element,L1)){
       append(L2->Next->Element,&L1);
@@ -69,25 +70,25 @@ List unionSet(List L1, List L2){
   return L1;
 }
 
-void deleteList(Position p, List *L){
+void deleteList(Position p, List *L){ // Xoá phần tử tại vị trí P
   Position T = (struct Node *)malloc(sizeof(struct Node));
   //makenullList(&T); 
   T = p->Next;
   p->Next = T->Next;
   free(T);
 }
-void deleteFirst(ElementType x, List *L){
+void deleteFirst(ElementType x, List *L){ // Xoá phần tử x đầu tiên xuất hiện
   Position p = locate(x,*L);
   deleteList(p,L);
 }
-void showResult(List L){
+void showResult(List L){ // Hiển thị danh sách
   while(L ->Next != NULL){
     printf("%d ",L->Next->Element);
     L = L->Next;
   }
   printf("\n");
 }
-void sort(List *L){
+void sort(List *L){ // Sắp xếp danh sách tăng dần
     Position p = (*L);
   while(p->Next != NULL){
       Position q = p->Next;
@@ -105,7 +106,7 @@ void sort(List *L){
 }
 }
 
-void normalize(List *L){
+void normalize(List *L){ // Xoá phần tử trùng
   Position p = (*L);
   while(p->Next != NULL){
     Position q = p->Next;
@@ -123,7 +124,7 @@ void normalize(List *L){
   
 }
 
-void copyEvenNumbers(List L1, List *L2){
+void copyEvenNumbers(List L1, List *L2){ // Copy số chẵn 
   makenullList(L2);
   while(L1->Next != NULL){
     if(L1->Next->Element % 2 == 0)
@@ -131,7 +132,7 @@ void copyEvenNumbers(List L1, List *L2){
     L1 = L1->Next;
   }
 }
-List difference(List L1, List L2){
+List difference(List L1, List L2){ // Hiệu 2 tập hợp
   List L3;
   makenullList(&L3);
   while(L1->Next != NULL){
--- CT177/x.c
+++ CT177/x.c
@@ -1,71 +1,27 @@
-#include <stdio.h>
-typedef struct{
-    int Elements[100];
-    int Last;
-} List;
-void makenullList(List *L){
-    L->Last = 0;
-}
-int members(int x, List L){
-    for(int i=0; i<L.Last;i++)
-        if(x == L.Elements[i])
-            return 1;
-    return 0;
-}
-void insertList(int x, List *L){
-    L->Elements[L->Last] = x;
-    L->Last ++;
-}
-void sort(List *L){
-    for(int i = 0 ; i < L->Last;i++){
-        for(int j =i+1 ; j < L->Last;j++){
-            if(L->Elements[i] > L->Elements[j]){
-                int temp = L->Elements[i];
-                L->Elements[i] = L->Elements[j];
-                L->Elements[j] = temp;
-            }
-        }
+#include "linked_list.c"
 
+void showReverse1(List *L){
+    Position p = (*L)->Next;
+    while(p->Next != NULL){
+        p = p->Next;
     }
-}
-void showList(List L){
-    for(int i = 0 ;i < L.Last;i++)
-        printf("%d ", L.Elements[i]);
-    printf("\n");
-}
-void Hop(List L, List L1, List *L2){
-    makenullList(L2);
-   *L2 = L;
-    for(int i = 0 ;i < L1.Last;i++)
-        if(!members(L1.Elements[i],*L2 ))
-            insertList(L1.Elements[i],L2);
-
+    (*L)->Next = p;
+    printf("%d ", (*L)->Next->Element);
+    free(p);
 }
 int main(){
-    int n;
-    scanf("%d",&n);
+    int x = 5;
     List L;
-    int x;
     makenullList(&L);
-    for(int i=0;i <n;i++){
-        scanf("%d",&x);
-        if(!members(x,L))
-            insertList(x,&L);
-    }
-    showList(L);
-    List L1;
-    makenullList(&L1);
-    scanf("%d",&n);
-    for(int i = 0;i <n;i++){
-        scanf("%d",&x);
-        if(!members(x,L1))
-            insertList(x,&L1);
-    }
-    showList(L1);
-    List L2;
-    Hop(L,L1,&L2);
-    showList(L2);
+    for(int i = 0 ; i  < x; i++){
+        int n;
+        scanf("%d",&n);
+        append(n,&L);
 
-    
-    return 0; // return
+    }
+    printf("\n");
+    showReverse1(&L);
+    printf("\n");
+    showResult(L);
+    return 0;
 }
