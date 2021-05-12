01141df4e86d0abdee36314f2ad88192b859e662
Caophuoclong <caophuoclong1@gmail.com>
2021-05-12 12:09:04 +0700

    Xin chao

--- CT174/Buoi 1/bubble_sort.c
+++ CT174/Buoi 1/bubble_sort.c
@@ -1,5 +1,13 @@
+#include <stdio.h>
+void swap(int *x, int *y){
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
 void bubble_sort(int arr[],int n){
     int i,j;
+    n = 12;
 	for(i = 0 ;i <= n -2  ; i++){
         for (j = n - 1 ; j >= i+1; j--){
             if(arr[j] < arr[j-1]){
@@ -8,4 +16,28 @@ void bubble_sort(int arr[],int n){
         }
     }
 
+}
+
+void print(int arr[]){
+    for(int i = 0 ;i < 12; i++){
+        printf("%d ",arr[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+    int n;
+    int arr[100];
+    freopen("dayso.inp", "r", stdin);
+    scanf("%d",&n);
+    for (int i = 0 ; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+    printf("%d\n", n);
+    print(arr);
+    bubble_sort(arr,n);
+    print(arr);
+
+    return 0;
+    
 }
