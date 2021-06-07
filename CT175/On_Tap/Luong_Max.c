2f7a2254f1697a9235aec797feb9336d2e3101c2
Phuoc Long <caophuoclong1@gmail.com>
2021-06-07 14:51:33 +0700

    Fix luong_max

--- CT175/On_Tap/Luong_Max.c
+++ CT175/On_Tap/Luong_Max.c
@@ -82,6 +82,48 @@ List neighbors(Graph *G, ElementType x){
 	return L;
 
 }
+List BFS(Graph *G, ElementType x){
+	List list;
+	make_null_list(&list);
+	Queue frontier;
+	make_null_queue(&frontier);
+	if (Mark[x] == 0)
+		push_queue(&frontier, x);
+	while(!empty_queue(&frontier)){
+		ElementType u = top_queue(&frontier);
+		pop_queue(&frontier);
+		push_list(&list, u);
+		Mark[u] = 1;
+		List L = neighbors(G,u);
+		for(int i = 0; i < L.size; i++){
+			ElementType y = L.data[i];
+			if(Mark[y] == 0)
+				push_queue(&frontier,y);
+			Mark[y] = 1;
+		}
+
+
+	}
+	return list;
+}
+int FordFullkerson(Grap *G, int s, int t){
+	int_flow(G);
+	Queue Q;
+
+	while(1){
+		Label labels[G->n];
+		lables[s].dir = 1;
+		labels[s].pre = s;
+		laebls[s].sigma = 100000;
+		make_null_queue(&Q);
+		push_queue(&Q,s);
+		while(!empty_queue(&Q)){
+
+		}
+
+
+	}
+}
 
 int main(){
 	int n, m, u,v,w;
