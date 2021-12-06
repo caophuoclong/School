ab05e8ac1c8739d45a38f5f93bc15ec74628dad5
caophuoclong <caophuoclong1@gmail.com>
2021-12-06 15:03:31 +0700

    Buoi 1

---
+++ CT188/index.js
@@ -0,0 +1,9 @@
+
+// $(document).ready(function () {
+
+// })
+// function readMore(n) {
+//     const moreText = $(".more")[n - 1]
+//     $(moreText).css("display", "inline-block");
+//     $(".btnReadmore")[n - 1].style.display = "none";
+// }
